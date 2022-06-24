d50869c2fb6ec678e30f193b13974b45698675ad
Sabiha Goycincik <sabiha.ozkan@getir.com>
2022-06-24 09:06:24 +0300

    minor fix

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Crypto.Cipher import DES
+# from Crypto.Cipher import DES
 
 
 from des import DES as MyDES
@@ -7,15 +7,15 @@ from helpers import Helpers
 
 if __name__ == '__main__':
     key = "secret_k"
-    text = "Hello world today is my first day"
+    text = "Hello world"
     my_des = MyDES()
-    des = DES.new(key, DES.MODE_ECB)
-    encrypted_text = des.encrypt(Helpers.add_padding(text))
+    # des = DES.new(key, DES.MODE_ECB)
+    # encrypted_text = des.encrypt(Helpers.add_padding(text))
 
     my_encrypted_text = my_des.encrypt(key, text, padding=True)
     my_decrypted_text = my_des.decrypt(key, my_encrypted_text)
     print("Encrypted by my algorithm: ", my_encrypted_text)
     print("Decrypted by my algorithm: ", my_decrypted_text)
 
-    print("Encrypted", encrypted_text)
-    print("Decrypted", des.decrypt(encrypted_text))
+    # print("Encrypted", encrypted_text)
+    # print("Decrypted", des.decrypt(encrypted_text))
